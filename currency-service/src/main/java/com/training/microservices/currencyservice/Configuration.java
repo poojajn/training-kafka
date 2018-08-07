7f0968f94192502d19baa2ea4bab147465738bbd
ramanakamadi <ramana.k@exilant.com>
2018-08-07 09:52:42 +0530

    2nd

---
+++ currency-service/src/main/java/com/training/microservices/currencyservice/Configuration.java
@@ -0,0 +1,35 @@
+package com.training.microservices.currencyservice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("currency-service")
+public class Configuration {
+
+	private int inr;
+	private int usd;
+	
+	public Configuration() {}
+	
+	public Configuration(int inr, int usd) {
+		super();
+		this.inr = inr;
+		this.usd = usd;
+	}
+	
+	public int getInr() {
+		return inr;
+	}
+	public void setInr(int inr) {
+		this.inr = inr;
+	}
+	public int getUsd() {
+		return usd;
+	}
+	public void setUsd(int usd) {
+		this.usd = usd;
+	}
+	
+	
+}
---
+++ currency-service/src/main/java/com/training/microservices/currencyservice/CurrencyConfigurationController.java
@@ -0,0 +1,20 @@
+package com.training.microservices.currencyservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyConfigurationController {
+    
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/currency")
+	public Configuration retriveCurrencyConfiguration() {
+		
+		return new Configuration(configuration.getInr(),configuration.getUsd());
+	}
+	
+	
+}
